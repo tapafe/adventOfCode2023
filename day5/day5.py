c19d504c55712c44c5ec9147407712cb0d5de7b9
tapafe <arnaud.rigaut@viacesi.fr>
2023-12-26 09:57:48 +0100

    Day 5 complete

--- day5/day5.py
+++ day5/day5.py
@@ -30,25 +30,36 @@ def part2(input_file):
         x2 = x1 + dx
         seeds_intervals.append((x1, x2, 1))
 
-    print(seeds_intervals)
     min_location = float('inf')
-
     while seeds_intervals:
-        print(seeds_intervals)
-        seeds_destination, seeds_source, level = seeds_intervals.pop()
 
-        if level == 8: # level 8 correspond humidity-to-location map:
-            print("azpkeazpaz")
-            min_location = 1
-            return min_location
+        x1, x2, level = seeds_intervals.pop()
+
+        if level == 8:  # level 8 correspond humidity-to-location map:
+            min_location = min(x1, min_location)
+            continue
 
         for conversion in re.findall(r'(\d+) (\d+) (\d+)', input[level]):
-            destination, source, range_length = map(int, conversion)
-            y2 = source + range_length
-            diff = destination - source
+            z, y1, dy = map(int, conversion)
+            y2 = y1 + dy
+            diff = z - y1
+            if x2 <= y1 or y2 <= x1:  # no overlap
+                continue
+            if x1 < y1:  # split original interval at y1
+                seeds_intervals.append((x1, y1, level))
+                x1 = y1
+            if y2 < x2:  # split original interval at y2
+                seeds_intervals.append((y2, x2, level))
+                x2 = y2
+            seeds_intervals.append(
+                (x1 + diff, x2 + diff, level + 1))  # perfect overlap -> make conversion and let pass to next nevel
+            break
 
-            seeds_intervals.append((seeds_destination + diff, seeds_source + diff, level + 1))
+        else:
+            seeds_intervals.append((x1, x2, level + 1))
+
+    return min_location
 
 
 # print(part1(open("input").read().strip()))
-part2(open("input").read().strip())
+print(part2(open("input").read().strip()))
