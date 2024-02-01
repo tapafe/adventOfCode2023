bea11d513befa0e66738f70b6a14e73d6428309c
tapafe <arnaud.rigaut@viacesi.fr>
2024-02-01 12:48:14 +0100

    Day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,6 +1,4 @@
-import re
-from collections import Counter
-from functools import cmp_to_key
+import math
 from itertools import cycle
 
 input_day8 = open("input").read().strip()
@@ -35,9 +33,35 @@ def part1(input_string):
     return steps
 
 
-def part2(input_list):
-    pass
+def part2(input_string):
+    directions, connections = input_string.split('\n\n')
+    directions = cycle('L' if d == 'L' else 'R' for d in directions)
+
+    road_to_follow = {'L': {}, 'R': {}}
+    starting_nodes = []
+    for line in connections.split('\n'):
+        source, left_right_destination = line.split('=')
+        source = source.strip()
+        if source[2] == 'A':
+            starting_nodes.append(source)
+        left_destination, right_destination = left_right_destination.split(",")
+        left = left_destination.strip()[1:].strip()
+        right = right_destination[:-1].strip()
+
+        road_to_follow['L'][source] = left
+        road_to_follow['R'][source] = right
+
+    cycles = []
+    for node in starting_nodes:
+        for steps, direction in enumerate(directions, start=1):
+            node = road_to_follow[direction][node]
+            if node[2] == 'Z':
+                cycles.append(steps)
+                break
+
+    print(cycles)
+    print(math.lcm(*cycles))
 
 
-print(part1(input_day8))
-# print(part2(input_day7))
+# print(part1(input_day8))
+print(part2(input_day8))
